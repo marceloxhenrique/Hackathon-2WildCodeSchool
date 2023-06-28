4d56f69bbf61c4b273f042052c2074200f249825
anthonydscrs <[andiscours@laposte.net](mailto:andiscours@laposte.net)>
2023-06-28 13:19:51 +0200

    setup project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
